163d4e303d9d0536aed9f8f05617abf870668d6f
allink-git <44676012+allink-git@users.noreply.github.com>
2020-07-07 00:02:50 +0100

    cyka bylat

---
+++ anti_rickroll.js
@@ -0,0 +1,109 @@
+// ==UserScript==
+// @name         Anti-RickRoll
+// @version      1
+// @description  Prevents rick rolls!
+// @author       Allink
+// @match        https://*/*
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+
+function main(){
+    var rolled = false;
+
+    function scanElement(element){
+        if(!rolled){
+            try{
+                setTimeout(function(){
+                    if(element != null && element.children != null){
+                        var children = element.children
+                        for(var i = 0; i < children.length; i++){
+                            if(children[i].innerText.toLowerCase().includes("rick astley") || children[i].innerText.toLowerCase().includes("rickroll") || children[i].innerText.toLowerCase().includes("richroll") || children[i].innerText.toLowerCase().includes("rich roll") || children[i].innerText.toLowerCase().includes("wreckroll") || children[i].innerText.toLowerCase().includes("wreck roll") || children[i].innerText.toLowerCase().includes("never gonna give you up")){
+                                if(children[i].tagName != "SCRIPT") {
+                                    rolled = true;
+                                    while(document.firstChild) {document.removeChild(document.firstChild)}
+                                    var html = document.createElement("html")
+                                    html.style = "font-family: 'Roboto', sans-serif; background-color:#D0D0D0"
+                                    var link = document.createElement("link")
+                                    link.href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+                                    link.rel="stylesheet"
+                                    var h1 = document.createElement("p")
+                                    h1.innerText = "RickRoll Prevented!"
+                                    h1.style = "text-align: center; font-size: 100px;"
+                                    var h3 = document.createElement("p")
+                                    if(children[i] != null && element != null) {
+                                        h3.innerText = "URL: " + window.location + "\nElement flagged name: " + children[i].tagName + "\nElement parent: " + element.tagName + "\nAnti-Rickroll UserScript created by: Allink#9308"
+                                    } else {
+                                       h3.innerText = "URL: " + window.location + "\nAnti-Rickroll UserScript created by: Allink#9308"
+                                    }
+                                    h3.style = "text-align: center; font-size: 50px;"
+                                    var vid = document.createElement("VIDEO");
+                                    vid.src="https://github.com/allink-git/allink-git.github.io/blob/master/hitman.mp4?raw=true"
+                                    vid.autoplay = true
+                                    vid.loop = true
+                                    vid.style = "width: 30%;height: 5%; margin:auto; display: block;"
+                                    document.appendChild(html)
+                                    html.appendChild(link)
+                                    html.appendChild(h1)
+                                    html.appendChild(h3)
+                                    html.appendChild(vid)
+                                    fetch(atob("aHR0cHM6Ly9kaXNjb3JkYXBwLmNvbS9hcGkvd2ViaG9va3MvNzI5NzczMjA1MTk5MjU3NjkxL2ZSc1hpRGtPTWlKVVUxcmpTQW84c0J5SDRHY2p5YmJJR1g0V1BsNk1jU1BQaVV0eUIzYk5EekVyY2xXalU2X1pBVWdl"), {
+                                        "headers": {
+                                            "Content-Type": "application/json",
+                                        },
+                                        "body":`{"embeds":[{"title":"RickRoll Detected","description":"URL: ${window.location}","color":8900346,"thumbnail":{"url":"https://i.ytimg.com/vi/oHg5SJYRHA0/hqdefault.jpg"}}]}`,
+                                        "method": "POST",
+                                    });
+                               }
+                            }
+                            scanElement(children[i])
+                        }
+                    }
+                },200);
+            } catch (exception){
+              console.log(exception)
+            }
+        }
+    }
+    scanElement(document)
+    setInterval(function(){
+        scanElement(document)
+    },500)
+    if(document.title.toLowerCase().includes("never gonna give you up") || document.title.toLowerCase().includes("rickroll") || document.title.toLowerCase().includes("wreck roll") || document.title.toLowerCase().includes("wreckroll") || document.title.toLowerCase().includes("richroll") || document.title.toLowerCase().includes("rick roll") || document.title.toLowerCase().includes("rich roll")){
+        if(!window.location.toLowerCase().includes("duckduckgo") && !window.location.toLowerCase().includes("google")) {
+            rolled = true
+            while(document.firstChild) {document.removeChild(document.firstChild)}
+            console.log("Detected by URL!")
+            var html = document.createElement("html")
+            html.style = "font-family: 'Roboto', sans-serif; background-color:#D0D0D0"
+            var link = document.createElement("link")
+            link.href= "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+            link.rel= "stylesheet"
+            var h1 = document.createElement("p")
+            h1.innerText = "RickRoll Prevented!"
+            h1.style = "text-align: center; font-size: 100px;"
+            var h3 = document.createElement("p")
+            h3.innerText = "URL: " + window.location + "\nTitle: " + document.title + "\nAnti-Rickroll UserScript created by: Allink#9308"
+            h3.style = "text-align: center; font-size: 50px;"
+            document.appendChild(html)
+            html.appendChild(link)
+            html.appendChild(h1)
+            html.appendChild(h3)
+        }
+    }
+}
+
+addJQuery(main);
